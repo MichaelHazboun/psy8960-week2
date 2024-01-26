982a11305eb11ce6f66908bae352a631dfc70eb9
Michael Hazboun <hazbo002@umn.edu>
2024-01-26 17:05:02 -0600

    Added the factor function and made things work for line 3 (and tested it out then deleted my testings)

--- R/week2.R
+++ R/week2.R
@@ -1,2 +1,3 @@
 # Import and Labeling
 rt_df<-read.csv(file= "~/psy8960-week2/data/week2.csv",header=TRUE)
+rt_df$condition <- factor(rt_df$condition, levels = c("A","B"), labels = c("Control", "Experimental"))
