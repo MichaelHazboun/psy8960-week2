b39b50471bfd3736d9f6c5c4556327419d2b6073
Michael Hazboun <hazbo002@umn.edu>
2024-01-27 16:26:17 -0600

    Added a single space after analysis so I can make a commit that says "Assignment Complete!"

--- R/week2.R
+++ R/week2.R
@@ -3,7 +3,7 @@ rt_df<-read.csv(file= "~/psy8960-week2/data/week2.csv",header=TRUE)
 rt_df$condition <- factor(rt_df$condition, levels = c("A","B"), labels = c("Control", "Experimental"))
 rt_df$gender <- factor(rt_df$gender, levels = c("M","F","N"), labels = c("Male","Female", "Nonbinary") )
 
-# Analysis
+# Analysis 
 (mean(rt_df$rt))
 rt_f_df<- subset(rt_df, subset = gender == "Female")
 hist(rt_f_df$rt)
