1f100b03e66e4e785b505a01a005dd5efd637f03
Michael Hazboun <hazbo002@umn.edu>
2024-01-29 14:16:46 -0600

    notes after class

--- R/week2.R
+++ R/week2.R
@@ -8,4 +8,7 @@ rt_df$gender <- factor(rt_df$gender, levels = c("M","F","N"), labels = c("Male",
 rt_f_df<- subset(rt_df, subset = gender == "Female")
 hist(rt_f_df$rt)
 datasets<-list(rt_df,rt_f_df)
-datasets[[1]][2]
+datasets[[1]][2] #this results in a data set. If I switched it with [[2]] at the end it extracts it as a vector
+# [[]] means extract
+# [] means subset
+# $ means extract
